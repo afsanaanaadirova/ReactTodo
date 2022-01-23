8e28f8ea569da6575ce25a2f5a0d30b27ead92de
afsananadirova <afsanan@crocusoft.com>
2022-01-23 20:03:38 +0400

    last commit-v1

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 
-function Todo({ text, todo, setTodos, completed, id, setCount, count }) {
+function Todo({ text, todo, setTodos, completed, id }) {
   const [number, setNumber] = useState(0);
 
   useEffect((e) => {
@@ -9,9 +9,6 @@ function Todo({ text, todo, setTodos, completed, id, setCount, count }) {
   }, []);
 
   const handlerDeleteOneByeOne = () => {
-    setCount((count) =>
-      todo.find((el) => el.id === id).completed ? count - 1 : count
-    );
     setTodos(todo.filter((element) => element.id !== id));
   };
   //complete todo
@@ -19,11 +16,6 @@ function Todo({ text, todo, setTodos, completed, id, setCount, count }) {
     setTodos(
       todo.map((item) => {
         if (item.id === id) {
-          if (!item.completed) {
-            setCount(count + 1);
-          } else {
-            setCount(count - 1);
-          }
           return {
             ...item,
             completed: !item.completed,
@@ -34,17 +26,23 @@ function Todo({ text, todo, setTodos, completed, id, setCount, count }) {
       })
     );
   };
+
   return (
-    <li className={`todo-item ${completed ? "completed" : ""}`}>
-      <span>
-        <div className="checkbox">
-          <input type="checkbox" name="" id={number} />
-          <label htmlFor={number} onClick={completeHandler}></label>
-        </div>
-        {text}
-      </span>
-      <FaRegTrashAlt className="trash-icon" onClick={handlerDeleteOneByeOne} />
-    </li>
+    <div>
+      <li className={`todo-item ${completed ? "completed" : ""}`}>
+        <span>
+          <div className="checkbox">
+            <input type="checkbox" name="" id={number} />
+            <label htmlFor={number} onClick={completeHandler}></label>
+          </div>
+          {text}
+        </span>
+        <FaRegTrashAlt
+          className="trash-icon"
+          onClick={handlerDeleteOneByeOne}
+        />
+      </li>
+    </div>
   );
 }
 
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,16 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoForm from "./TodoForm";
 import Todo from "./Todo";
+import noTask from "../assests/images/noTask.png";
 
 function Todos() {
   const [todo, setTodos] = useState([]);
   const [inputText, setInputText] = useState("");
-  const [count, setCount] = useState(0);
 
   //delete all todos
   const handlerDelete = () => {
     setTodos([]);
-    setCount(0);
+  };
+  //RUN ONCE when the app start
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    saveLocalTodos(todo);
+  }, [todo]);
+
+  //Save to Local Store
+  const saveLocalTodos = (todo) => {
+    localStorage.setItem("todo", JSON.stringify(todo));
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todo") === null) {
+      localStorage.setItem("todo", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todo"));
+      setTodos(todoLocal);
+    }
   };
   return (
     <div>
@@ -21,24 +41,31 @@ function Todos() {
         setInputText={setInputText}
       />
       <ul className="todo-list">
-        {todo &&
+        {todo.length <= 0 ? (
+          <div className="emptyTodo">
+            <img src={noTask} alt="" />
+            <p>Heç bir tapşırıq yoxdur</p>
+          </div>
+        ) : (
+          todo &&
           todo.map((element, index) => (
             <Todo
               key={index}
               text={element.text}
               id={element.id}
-              count={count}
-              setCount={setCount}
               completed={element.completed}
               todo={todo}
               setTodos={setTodos}
             />
-          ))}
+          ))
+        )}
       </ul>
       <div className="footer">
         <span>
           <span>Ümumi {todo.length} tapşırıq</span>
-          <span>Hazır: {count} tapşırıq</span>
+          <span>
+            Hazır: {todo.filter((x) => x.completed === true).length} tapşırıq
+          </span>
         </span>
         <button onClick={handlerDelete}>Hamısını sil</button>
       </div>
